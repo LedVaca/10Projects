9884d3db5f5e305a3cf16263e1ede33e7c7f73df
LedVaca <luisrazo@live.com.mx>
2017-09-16 19:41:21 -0500

    archivos
    Subí todos los archivos

---
+++ Assets/Pong/Scripts/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+	private int count;
+	// Use this for initialization
+	void Start () {
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("Punto")) {
+			other.gameObject.SetActive (false);
+		}
+	}
+}
