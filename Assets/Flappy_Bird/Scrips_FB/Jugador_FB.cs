cc0ead0b07b62f541483c6675cb7eb4d87570775
Usuario <luisrazo@live.com.mx>
2017-11-09 21:49:35 -0600

    flappy
    Avance_ya brinca

---
+++ Assets/Flappy_Bird/Scrips_FB/Jugador_FB.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador_FB : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	public	float jumpForce = 0;
+
+
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// jump
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			rb2d.AddForce (Vector2.up *jumpForce);
+
+			Debug.Log ("brica");
+
+		}
+	}
+}
